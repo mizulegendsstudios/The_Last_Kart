d8b9ad38ddcf60063fd7fe40da26539896756bc2
mizulegendsstudios <mizulegendsgg@gmail.com>
2025-08-08 23:38:49 +0000

    Create track.js

---
+++ scripts/track.js
@@ -0,0 +1,11 @@
+export class Track {
+  constructor(shape) {
+    this.shape = shape; // 'X', 'O', 'triangle'
+    this.ramps = true;
+    this.crossings = true;
+  }
+
+  render() {
+    // Dibuja la pista en canvas o DOM
+  }
+}
