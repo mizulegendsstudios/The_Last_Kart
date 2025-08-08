19286146b0644251294c63513f58464e694d8206
mizulegendsstudios <mizulegendsgg@gmail.com>
2025-08-08 23:45:56 +0000

    Update transitions.js

--- scripts/transitions.js
+++ scripts/transitions.js
@@ -1,3 +1,9 @@
+/*
+    Copyright (C) 2023 Moises Núñez
+    Este archivo es parte de The Last Kart.
+    The Last Kart está licenciado bajo la GNU Affero General Public License v3.0. 
+*/
+
 export function fadeOut(element, callback) {
     element.style.opacity = '0';
     setTimeout(() => {
